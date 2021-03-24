b28fa8cf2700f6f35a57a9e6e56a163336fdceaf
Vinicius Sebba Patto <viniciusspatto@MacBook-Pro-de-Vinicius-2.local>
2021-03-23 21:31:34 -0300

    initial test / load

--- NPL_BoW_spacy.py
+++ NPL_BoW_spacy.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 # Loading the spam data
 # ham is the label for non-spam messages
-spam = pd.read_csv('../input/nlp-course/spam.csv')
+spam = pd.read_csv('../nlp-course/spam.csv')
 spam.head(10)
 print(spam.head())
